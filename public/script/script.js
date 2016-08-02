078f12ec2032f8f7804a32545324486448aa3e32
Aleš Mráz <alik810@seznam.cz>
2016-08-02 13:05:34 +0200

    Add main file

---
+++ public/script/script.js
@@ -0,0 +1,14 @@
+function dropdownClick(target, other) {
+  if($(other).hasClass('active')) {
+    $(other).removeClass('active');
+  }
+  $(target).toggleClass('active');
+}
+
+$('.dropdown-trigger').on('click', function() {
+  dropdownClick('.profile-dropdown', '.mobile-menu-dropdown');
+});
+
+$('.dropdown-trigger--mobile').on('click', function() {
+  dropdownClick('.mobile-menu-dropdown', '.profile-dropdown');
+});
